e0ac8c449d72850bea2758ae015033e78bf79e1a
TwoBitCliff <j-lithgow@live.co.uk>
2021-02-06 17:37:01 +0000

    Created Sign Up Functionality

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import (
     Flask, flash, render_template,
-     redirect, request, session, url_for)
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -15,7 +15,7 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo=PyMongo(app)
+mongo = PyMongo(app)
 
 
 @app.route("/")
@@ -27,6 +27,31 @@ def get_squad():
 
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
+
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"email": request.form.get("email").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("sign_up"))
+
+        sign_up = {
+            "email": request.form.get("email").lower(),
+            "password": generate_password_hash(request.form.get("password")),
+            "first_name": request.form.get("first_name").capitalize(),
+            "last_name": request.form.get("last_name").capitalize(),
+            "contact_number": request.form.get("contact_number"),
+            "team_name": request.form.get("team_name").capitalize()
+        }
+        mongo.db.users.insert_one(sign_up)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("email").lower()
+        flash("Registration Successful!")
+
+        session["user"] = request.form.get("first_name")
     return render_template("signup.html")
 
 
