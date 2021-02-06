ab3590adea687a2481906f759ba1fa6d0f91b689
TwoBitCliff <j-lithgow@live.co.uk>
2021-02-06 16:57:11 +0000

    Created Sign Up Template and Form

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
      redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,12 @@ mongo=PyMongo(app)
 def get_squad():
     squad = mongo.db.squad.find()
     return render_template("base.html", squad=squad)
+
+
+@app.route("/sign_up", methods=["GET", "POST"])
+def sign_up():
+    return render_template("signup.html")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
